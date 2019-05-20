972069811174319eff5548d713789ebd4ce185e4
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2019-05-20 17:09:21 -0500

    Calculator class and sum method
    We want to be able to follow the test - code - refactor process to
    do TDD.
    
    This implements the tests for math methods first and the the code
    for this to actually work and perform the sum action.
    
    And at the end the refactor process to make the code look better
    and more mantainable.

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,13 +5,25 @@ class Calculator
   def initialize(digits_string)
     @digits = parse_input(digits_string) 
   end
+  
+  def divide
+    digits.inject(:/)
+  end
 
   def parse_input(string)
     string.split(',').map{ |digit| digit.to_i } 
+  end 
+  
+  def multiply
+    digits.inject(:*)
   end
   
   def sum
     digits.inject(:+)
+  end
+
+  def substract
+    digits.inject(:-)
   end
 end
 
@@ -19,6 +31,21 @@ describe Calculator do
   it 'sums provided digits' do 
     calc = Calculator.new('1, 2, 3, 4, 5')
     calc.sum.must_equal(15)
+  end
+
+  it 'multiplies provided digits' do
+    calc = Calculator.new('4, 5')
+    calc.multiply.must_equal(20)
+  end
+
+  it 'substracts provided digits' do
+    calc = Calculator.new('30, 20, 5')
+    calc.substract.must_equal(5)
+  end
+
+  it 'divides provided digits' do
+    calc = Calculator.new('30, 5')
+    calc.divide.must_equal(6)
   end
 end 
 
